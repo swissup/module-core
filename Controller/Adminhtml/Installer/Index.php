22ac82456c6cd67f05ca4f75b30ddab9842b1439
Vova Yatsyuk <vovayatsyuk@users.noreply.github.com>
2016-04-19 17:08:15 +0300

    Unfinished work on swissup modules manager

---
+++ Controller/Adminhtml/Installer/Index.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Swissup\Core\Controller\Adminhtml\Installer;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    const ADMIN_RESOURCE = 'Swissup_Core::installer_index';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Swissup_Core::module_manager');
+        $resultPage->addBreadcrumb(__('Module Manager'), __('Module Manager'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Module Manager'));
+        return $resultPage;
+    }
+}
--- Model/ResourceModel/Module/RemoteCollection.php
+++ Model/ResourceModel/Module/RemoteCollection.php
@@ -64,15 +64,18 @@ class RemoteCollection extends \Magento\Framework\Data\Collection
         }
 
         try {
-            $client = $this->httpClientFactory->create();
-            $client->setUri($this->getFeedUrl());
-            $client->setConfig(array('maxredirects'=>5, 'timeout'=>30));
-            $client->setParameterGet('domain', $this->request->getHttpHost());
-            $responseBody = $client->request()->getBody();
-            $modules = $this->jsonHelper->jsonDecode($responseBody);
-            if (!is_array($modules)) {
-                throw new \Exception('Decoding failed');
-            }
+            $modules = $this->fetchPackages($this->getFeedUrl());
+            // do not use remote response
+            // Need to rewrite Subscription_Checker logic
+            $modules = array(
+                'Swissup_Subscription' => array(
+                    'code'          => 'Swissup_Subscription',
+                    'version'       => '',
+                    'link'          => 'https://swissuplabs.com',
+                    'download_link' => 'https://swissuplabs.com/subscription/customer/products/',
+                    'identity_key_link' => 'https://swissuplabs.com/license/customer/identity/'
+                )
+            );
         } catch (\Exception $e) {
             $modules = array(
                 'Swissup_Subscription' => array(
@@ -112,7 +115,66 @@ class RemoteCollection extends \Magento\Framework\Data\Collection
         return $this;
     }
 
-    protected function getFeedUrl()
+    protected function fetchPackages($url)
+    {
+        $client = $this->httpClientFactory->create();
+        $client->setUri($url);
+        $client->setConfig(array('maxredirects'=>5, 'timeout'=>30));
+        $client->setParameterGet('domain', $this->request->getHttpHost());
+        $responseBody = $client->request()->getBody();
+
+        $response = $this->jsonHelper->jsonDecode($responseBody);
+        if (!is_array($response)) {
+            throw new \Exception('Decoding failed');
+        }
+
+        if (!empty($response['packages'])) {
+            $modules = [];
+            foreach ($response['packages'] as $package => $info) {
+                $versions = array_keys($info);
+                $latestVersion = array_reduce(
+                    $versions,
+                    [$this, 'getNewerVersion']
+                );
+
+                $unset = [
+                    'autoload',
+                    'dist',
+                    'license',
+                    'require',
+                    'source',
+                    'support',
+                    'version_normalized'
+                ];
+                foreach ($unset as $key) {
+                    unset($info[$latestVersion][$key]);
+                }
+
+                $modules[$package] = $info[$latestVersion];
+                $modules[$package]['code'] = $package;
+            }
+            return $modules;
+        }
+
+        if (!empty($response['includes'])) {
+            // fetch for swissup.github.io/packages/include/all${sha1}.json
+            // sha1 is taken from swissup.github.io/packages/packages.json
+            $include = key($response['includes']);
+            return $this->fetchPackages($this->getFeedUrl($include));
+        }
+
+        throw new \Exception('No packages found in response');
+    }
+
+    protected function getNewerVersion($carry, $item)
+    {
+        if (version_compare($carry, $item) === -1) {
+            $carry = $item;
+        }
+        return $carry;
+    }
+
+    protected function getFeedUrl($suffix = 'packages.json')
     {
         $useHttps = $this->scopeConfig->getValue(
             self::XML_USE_HTTPS_PATH,
@@ -122,6 +184,6 @@ class RemoteCollection extends \Magento\Framework\Data\Collection
             self::XML_FEED_URL_PATH,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
-        return ($useHttps ? 'https://' : 'http://') . $url;
+        return ($useHttps ? 'https://' : 'http://') . $url . $suffix;
     }
 }
