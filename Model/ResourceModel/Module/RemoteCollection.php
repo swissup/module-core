fce4498d1705296a9f3c6d913fce026430c76e1d
Vova Yatsyuk <vovayatsyuk@users.noreply.github.com>
2016-04-25 16:11:42 +0300

    Theme code should include all parts of package name

--- Model/ResourceModel/Module/RemoteCollection.php
+++ Model/ResourceModel/Module/RemoteCollection.php
@@ -117,8 +117,6 @@ class RemoteCollection extends \Magento\Framework\Data\Collection
     protected function convertPackageNameToModuleName($packageName)
     {
         list($vendor, $name) = explode('/', $packageName, 2);
-        $name = str_replace('theme-adminhtml-', '', $name);
-        $name = str_replace('theme-frontend-', '', $name);
         $name = str_replace('-', ' ', $name);
         $name = str_replace(' ', '', ucwords($name));
         return ucfirst($vendor) . '_' . $name;
