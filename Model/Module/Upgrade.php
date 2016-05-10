0b7442904ff1a4c065de67466ddd88154c71ae4f
Vova Yatsyuk <vovayatsyuk@users.noreply.github.com>
2016-05-10 15:21:13 +0300

    RunCmsPage method added to backup and create new cms pages

--- Model/Module/Upgrade.php
+++ Model/Module/Upgrade.php
@@ -14,6 +14,43 @@ abstract class Upgrade implements ModuleUpgradeInterface
     protected $storeIds = array();
 
     /**
+     * Used to guarantee unique backup names in case of duplicate name and date
+     *
+     * @var int
+     */
+    protected static $backupIterator = 0;
+
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $objectManager;
+
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime\TimezoneInterface
+     */
+    protected $localeDate;
+
+    /**
+     * Store manager
+     *
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
+    ) {
+        $this->objectManager = $objectManager;
+        $this->localeDate = $localeDate;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
      * Additional operations could be done from this method
      */
     public function up()
@@ -42,6 +79,12 @@ abstract class Upgrade implements ModuleUpgradeInterface
 
     public function upgrade()
     {
+        foreach ($this->getOperations() as $operation => $instructions) {
+            $method = 'run' . ucfirst($operation);
+            if (method_exists($this, $method)) {
+                $this->$method($instructions);
+            }
+        }
         $this->up();
     }
 
@@ -79,5 +122,116 @@ abstract class Upgrade implements ModuleUpgradeInterface
     public function getMessageLogger()
     {
         return $this->installer->getMessageLogger();
+    }
+
+    /**
+     * Backup and create new cms pages
+     *
+     * @param  array $data Array of page data arrays
+     * <pre>
+     * [
+     *     'home' => [
+     *         'title' => 'Argento Essence',
+     *         'identifier' => 'home',
+     *         'page_layout' => '1column'
+     *         'content' => '',
+     *         'is_active' => 1
+     *     ]
+     * ]
+     * </pre>
+     * @return void
+     */
+    public function runCmspage($data)
+    {
+        // bugfix: cleanup links in url_rewrite table before creating new pages.
+        // Related to https://github.com/magento/magento2/issues/4113
+        $this->cleanupUrlRewrites(
+            'cms-page',
+            array_map(
+                function ($item) {
+                    return $item['identifier'];
+                },
+                $data
+            )
+        );
+
+        $isSingleStoreMode = $this->storeManager->isSingleStoreMode();
+        foreach ($data as $itemData) {
+            // 1. backup existing pages
+            $collection = $this->objectManager
+                ->create('Magento\Cms\Model\ResourceModel\Page\Collection')
+                ->addStoreFilter($this->getStoreIds())
+                ->addFieldToFilter('identifier', $itemData['identifier']);
+
+            foreach ($collection as $page) {
+                $page->load($page->getId()); // load stores
+                $storesToLeave = array_diff($page->getStoreId(), $this->getStoreIds());
+                if (count($storesToLeave) && !$isSingleStoreMode) {
+                    // unassign page from stores that will have new pages
+                    $page->setStores($storesToLeave);
+                } else {
+                    // disable page, because it has not store to assign to
+                    $page->setIsActive(0)
+                        ->setIdentifier($this->getBackupIdentifier($page->getIdentifier()));
+                }
+
+                try {
+                    $page->save();
+                } catch (\Magento\Framework\Exception\AlreadyExistsException $e) {
+                    // $this->_fault('cmspage_backup', $e);
+                } catch (Exception $e) {
+                    // $this->_fault('cmspage_backup', $e);
+                }
+            }
+
+            // 2. create new page
+            try {
+                $this->objectManager->create('Magento\Cms\Model\Page')
+                    ->setData($itemData)
+                    ->setStores($this->getStoreIds()) // see Magento\Cms\Model\ResourceModel\Page::_afterSave
+                    ->save();
+            } catch (\Magento\Framework\Exception\AlreadyExistsException $e) {
+                // $this->_fault('cmspage_save', $e);
+            } catch (Exception $e) {
+                // $this->_fault('cmspage_save', $e);
+            }
+        }
+    }
+
+    /**
+     * Removes url rewrite entries, that match requested conditions
+     *
+     * @param  string $entityType
+     * @param  array $requestPaths
+     * @return void
+     */
+    protected function cleanupUrlRewrites($entityType, $requestPaths)
+    {
+        $urlCollection = $this->objectManager
+            ->create('Magento\UrlRewrite\Model\ResourceModel\UrlRewriteCollection')
+            ->addFieldToFilter('entity_type', $entityType)
+            ->addFieldToFilter('request_path', ['in' => $requestPaths])
+            ->addFieldToFilter('store_id', ['in' => $this->getStoreIds()]);
+
+        foreach ($urlCollection as $item) {
+            $item->delete();
+        }
+    }
+
+    /**
+     * Returns unique string. Used to backup existing pages, blocks, etc
+     * This method is not 100% bullet proof, but there is very low chance to
+     * receive duplicate string.
+     *
+     * @param string $identifier
+     * @return string
+     */
+    protected function getBackupIdentifier($identifier)
+    {
+        return $identifier
+            . '_backup_'
+            . self::$backupIterator++
+            . '_'
+            . $this->localeDate->date()->format('Y-m-d-H-i-s');
     }
 }
