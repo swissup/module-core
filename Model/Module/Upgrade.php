93073845a0a3e4030cc5bbcde82547f939ac4d98
Vova Yatsyuk <vovayatsyuk@users.noreply.github.com>
2016-05-10 16:38:47 +0300

    runEasyslide method added

--- Model/Module/Upgrade.php
+++ Model/Module/Upgrade.php
@@ -260,6 +260,50 @@ abstract class Upgrade implements ModuleUpgradeInterface
     }
 
     /**
+     * Create new slider.
+     * If duplicate is found - do nothing.
+     *
+     * @param  array $data Array of slider data arrays
+     * @return void
+     */
+    public function runEasyslide($data)
+    {
+        foreach ($data as $itemData) {
+            $slider = $this->objectManager
+                ->create('Swissup\EasySlide\Model\Slider')
+                ->load($itemData['identifier'], 'identifier');
+            if ($slider->getId()) {
+                continue;
+            }
+
+            try {
+                $slider->setData($itemData)->save();
+            } catch (Exception $e) {
+                // $this->_fault('easyslide_slider_save', $e);
+                continue;
+            }
+
+            $slideDefaults = array(
+                'is_active'   => 1,
+                'target'      => '_self',
+                'description' => '',
+                'slider_id'   => $slider->getId()
+            );
+            foreach ($itemData['slides'] as $slide) {
+                try {
+                    $this->objectManager
+                        ->create('Swissup\EasySlide\Model\Slides')
+                        ->setData(array_merge($slideDefaults, $slide))
+                        ->save();
+                } catch (Exception $e) {
+                    // $this->_fault('easyslide_slide_save', $e);
+                    continue;
+                }
+            }
+        }
+    }
+
+    /**
      * Removes url rewrite entries, that match requested conditions
      *
      * @param  string $entityType
