61c5febed915a0e2d40f6207bd3fa3bf07727277
Vova Yatsyuk <vovayatsyuk@users.noreply.github.com>
2016-05-13 12:02:02 +0300

    Dummy easybanner command added. Waiting for stable release

---
+++ Model/Module/UpgradeCommands/Easybanner.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Swissup\Core\Model\Module\UpgradeCommands;
+
+class Easybanner extends \Swissup\Core\Model\Module\UpgradeCommands\AbstractCommand
+{
+    /**
+     * Create new banners and plceholders.
+     *
+     * If duplicates are found - do nothing.
+     *
+     * @param  array $data Array of placeholders/banner data arrays
+     * @return void
+     */
+    public function execute($data)
+    {
+        //
+    }
+}
