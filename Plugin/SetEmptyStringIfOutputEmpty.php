8c420b4c16fba754358530a28c1e47f84437bcb8
Vitaliy <vitaliy@mage2cloud.com>
2018-03-12 16:36:37 +0200

    add plugin for AbstractProduct block - return empty string when output is empty

---
+++ Plugin/SetEmptyStringIfOutputEmpty.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Swissup\Core\Plugin;
+
+class SetEmptyStringIfOutputEmpty
+{
+        /**
+         * After plugin for toHtml method of layout block (applicable for any block)
+         *
+         * @param  \Magento\Framework\View\Element\AbstractBlock $subject
+         * @param  string $resultHtml
+         * @return string
+         */
+        public function afterToHtml(
+            \Magento\Framework\View\Element\AbstractBlock $subject,
+            $resultHtml
+        ) {
+            $html = trim(preg_replace('/\s+/', ' ', $resultHtml));
+            return empty($html) ? $html : $resultHtml;
+        }
+}
