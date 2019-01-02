1b977c9750646798457747c0a01d304c1e17f928
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2019-01-02 11:12:05 +0200

    Fixed backend error, when AdminNotification module is not installed

--- Observer/Backend/FetchNotifications.php
+++ Observer/Backend/FetchNotifications.php
@@ -15,15 +15,23 @@ class FetchNotifications implements \Magento\Framework\Event\ObserverInterface
     protected $backendAuthSession;
 
     /**
+     * @var \Magento\Backend\Helper\Data
+     */
+    protected $helper;
+
+    /**
      * @param \Swissup\Core\Model\Notification\FeedFactory $feedFactory
      * @param \Magento\Backend\Model\Auth\Session $backendAuthSession
+     * @param \Magento\Backend\Helper\Data $helper
      */
     public function __construct(
         \Swissup\Core\Model\Notification\FeedFactory $feedFactory,
-        \Magento\Backend\Model\Auth\Session $backendAuthSession
+        \Magento\Backend\Model\Auth\Session $backendAuthSession,
+        \Magento\Backend\Helper\Data $helper
     ) {
         $this->feedFactory = $feedFactory;
         $this->backendAuthSession = $backendAuthSession;
+        $this->helper = $helper;
     }
 
     /**
@@ -34,6 +42,10 @@ class FetchNotifications implements \Magento\Framework\Event\ObserverInterface
      */
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
+        if (!$this->helper->isModuleOutputEnabled('Magento_AdminNotification')) {
+            return;
+        }
+
         if ($this->backendAuthSession->isLoggedIn()) {
             $feedModel = $this->feedFactory->create();
             $feedModel->checkUpdate();
