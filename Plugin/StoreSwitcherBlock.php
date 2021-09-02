ece9d31d14637dc77f3acfc6a4678103761c9a48
Roman Chumak <chumakrom@gmail.com>
2021-09-02 11:56:23 +0300

    Setting view model for store switcher block with plugin to be able using it as a widget

---
+++ Plugin/StoreSwitcherBlock.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Swissup\Core\Plugin;
+
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Store\ViewModel\SwitcherUrlProvider as ViewModel;
+
+class StoreSwitcherBlock
+{
+    /**
+     * @var ViewModel|null
+     */
+    private $viewModel = null;
+
+    /**
+     * @param ObjectManagerInterface $objectManager
+     */
+    public function __construct(ObjectManagerInterface $objectManager)
+    {
+        if (class_exists(ViewModel::class)) {
+            $this->viewModel = $objectManager->create(ViewModel::class);
+        }
+    }
+
+    /**
+     * @param \Magento\Store\Block\Switcher $block
+     */
+    public function beforeToHtml(\Magento\Store\Block\Switcher $block)
+    {
+        if ($this->viewModel) {
+            $block->setViewModel($this->viewModel);
+            $block->assign('viewModel', $this->viewModel);
+        }
+    }
+}
