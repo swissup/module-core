4d733c119d51fcbbd608ce3a5deb0c4bd097a7aa
rommmka <rommmmka@gmail.com>
2023-04-11 11:57:32 +0300

    Applied code-review recommendations

--- Block/Adminhtml/Config/Form/Field/VirtualCheck.php
+++ Block/Adminhtml/Config/Form/Field/VirtualCheck.php
@@ -8,13 +8,13 @@ use Magento\Theme\Model\ResourceModel\Theme\Collection;
 
 class VirtualCheck extends \Magento\Config\Block\System\Config\Form\Field
 {
-
     protected $_template = 'config/field/virtual_check.phtml';
     
     public function render(AbstractElement $element)
     {
         $this->assign('configElement', $element);
         $html = $this->toHtml();
+        
         return $this->_decorateRowHtml($element, "<td class='themes-table' colspan=\"3\">$html</td>");
     }
 
@@ -22,6 +22,5 @@ class VirtualCheck extends \Magento\Config\Block\System\Config\Form\Field
     {
         return ObjectManager::getInstance()->create(Collection::class)
             ->addFieldToFilter('type', 1);
-
     }
 }
